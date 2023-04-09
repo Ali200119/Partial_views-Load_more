eea6f703d349e3756ad15d815f853c0bb9e8ef82
Ali200119 <aliit@code.edu.az>
2023-04-09 16:11:59 +0400

    feat: Backup file

--- PurpleBuzz/PurpleBuzz/Migrations/20230409120749_AddTables.Designer.cs
+++ PurpleBuzz/PurpleBuzz/Migrations/20230409120749_AddTables.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PurpleBuzz.Data;
 
@@ -10,9 +11,10 @@ using PurpleBuzz.Data;
 namespace PurpleBuzz.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230409120749_AddTables")]
+    partial class AddTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
